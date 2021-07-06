590377a54502dedf4afd54e0a8f10db71ec24d0c
yahya ganjo <yahya.ganjo@gmail.com>
2021-07-06 20:40:28 +0200

    project week1 finished

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -2,13 +2,34 @@
  * Exercise 3: Create an HTTP web server
  */
 
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 //create a server
 let server = http.createServer(function (req, res) {
   // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  if (req.url === "/") {
+    fs.readFile("index.html", (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    });
+  } else if (req.url === "/index.js") {
+    fs.readFile("index.js", (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+      res.end(data);
+    });
+  } else if (req.url === "/style.css") {
+    fs.readFile("style.css", (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404);
+    res.end("<h1>Page not found</h1>");
+  }
 });
 
 server.listen(3000); // The server starts to listen on port 3000
